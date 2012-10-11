5d62bd3390c72a62d9d06b7d67f4bfc0dd32333b
Geoff Willingham <geoff.willingham@paconsulting.com>
2012-10-11 17:17:10 +0100

    Additional pref for Widgets
    Additional pref - 'hidden' widgets can now 'reserve' screen space to
    draw themselves into when they get data

--- src/org/metawatch/manager/MetaWatchService.java
+++ src/org/metawatch/manager/MetaWatchService.java
@@ -225,6 +225,7 @@ public class MetaWatchService extends Service {
 		public static boolean showActionsInCall = true;
 		public static String themeName = "";
 		public static boolean hideEmptyWidgets = false;
+		public static boolean hiddenWidgetsReserveSpace = false;
 	}
 
 	public final class WatchType {
@@ -364,6 +365,8 @@ public class MetaWatchService extends Service {
 				Preferences.themeName);
 		Preferences.hideEmptyWidgets = sharedPreferences.getBoolean("HideEmptyWidgets",
 				Preferences.hideEmptyWidgets);
+		Preferences.hiddenWidgetsReserveSpace = sharedPreferences.getBoolean("HiddenWidgetsReserveSpace",
+				Preferences.hiddenWidgetsReserveSpace);
 		
 		boolean silent = sharedPreferences.getBoolean("SilentMode", silentMode );
 		if (silent!=silentMode)
