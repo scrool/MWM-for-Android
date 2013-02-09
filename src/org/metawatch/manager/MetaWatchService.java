0dee12c8c3c7782abb5b5f7b45b33688bb04f2e3
nseidm1 <nseidm1@192.168.0.219>
2013-02-08 22:25:02 -0500

    Ensure that an IDLE_NEXT_PAGE button press has the system in IDLE mode. Otherwise updateIdlePages will not refresh properly. Typically this isn't an issue, but a race condition sometimes occurs.

--- src/org/metawatch/manager/MetaWatchService.java
+++ src/org/metawatch/manager/MetaWatchService.java
@@ -1147,6 +1147,7 @@ public class MetaWatchService extends Service {
 
 		    case Idle.IDLE_NEXT_PAGE:
 			if (MetaWatchService.watchType == MetaWatchService.WatchType.DIGITAL) {
+			    Idle.getInstance().toIdle(this);
 			    Idle.getInstance().nextPage(this);
 			    Idle.getInstance().updateIdle(this, true);
 			}
